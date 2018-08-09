40491701b767e1323bc47a62fd0d1c09a26eb133
gimi65536 <gimi65536@hotmail.com>
2018-08-09 23:08:54 +0800

    增加權重與閾值的調整
    現在對已經從API得到聽力結果的音檔重複按recognize將直接回傳先前的結果而不會重複呼叫API
    重複按recognize的行為將視為用一個調整後的權重，重新算一次stick/anchor

--- web_recognize.py
+++ web_recognize.py
@@ -8,96 +8,101 @@ wit_key = gl.get_value("wit_key")
 houndify_id = gl.get_value("houndify_id")
 houndify_key = gl.get_value("houndify_key")
 
+store_result = {}
+
 def recognize(AudioFile):
-    audio_to_trans = AudioFile
-    no_exception = True
-    exceed_quota = False
+	if AudioFile in store_result:
+		return store_result[AudioFile]
+	audio_to_trans = AudioFile
+	no_exception = True
+	exceed_quota = False
 
-    # obtain audio from the microphone
-    import speech_recognition as sr
-    r = sr.Recognizer()
-    with sr.AudioFile(audio_to_trans) as source:
-        r.adjust_for_ambient_noise(source)
-        audio = r.record(source)
-        #print("-------------------------------")
+	# obtain audio from the microphone
+	import speech_recognition as sr
+	r = sr.Recognizer()
+	with sr.AudioFile(audio_to_trans) as source:
+		r.adjust_for_ambient_noise(source)
+		audio = r.record(source)
+		#print("-------------------------------")
 
-        try:
-            #有language參數
-            google_result = r.recognize_google(audio,language='en-US')
-            if google_result == '':
-                no_exception = False
-                google_result = "Exception: google cannot recognize!"
-                #print(google_result)
-            else:
-                #print("google : ", google_result)
-                pass
-        except:
-            no_exception = False
-            google_result = "Exception: google cannot recognize!"
-            #print(google_result)
+		try:
+			#有language參數
+			google_result = r.recognize_google(audio,language='en-US')
+			if google_result == '':
+				no_exception = False
+				google_result = "Exception: google cannot recognize!"
+				#print(google_result)
+			else:
+				#print("google : ", google_result)
+				pass
+		except:
+			no_exception = False
+			google_result = "Exception: google cannot recognize!"
+			#print(google_result)
 
-        try:
-            #password:b04@NTUIM,有language參數(沒有zh-TW)
-            ibm_result = r.recognize_ibm(audio,username=ibm_username,password=ibm_password,language='en-US')
-            if ibm_result == '':
-                no_exception = False
-                ibm_result = "Exception: ibm cannot recognize!"
-                #print(ibm_result)
-            else:
-                #print("ibm : ", ibm_result)
-                pass
-        except:
-            no_exception = False
-            ibm_result = "Exception: ibm cannot recognize!"
-            #print(ibm_result)
-            
-        try:
-            #在網頁裡面改語言
-            wit_result = r.recognize_wit(audio,key=wit_key)
-            if wit_result == '':
-                no_exception = False
-                wit_result = "Exception: wit cannot recognize!"
-                #print(wit_result)
-            else:
-                #print("wit : ", wit_result)
-                pass
-        except:
-            no_exception = False
-            wit_result = "Exception: wit cannot recognize!"
-            #print(wit_result)
+		try:
+			#password:b04@NTUIM,有language參數(沒有zh-TW)
+			ibm_result = r.recognize_ibm(audio,username=ibm_username,password=ibm_password,language='en-US')
+			if ibm_result == '':
+				no_exception = False
+				ibm_result = "Exception: ibm cannot recognize!"
+				#print(ibm_result)
+			else:
+				#print("ibm : ", ibm_result)
+				pass
+		except:
+			no_exception = False
+			ibm_result = "Exception: ibm cannot recognize!"
+			#print(ibm_result)
+			
+		try:
+			#在網頁裡面改語言
+			wit_result = r.recognize_wit(audio,key=wit_key)
+			if wit_result == '':
+				no_exception = False
+				wit_result = "Exception: wit cannot recognize!"
+				#print(wit_result)
+			else:
+				#print("wit : ", wit_result)
+				pass
+		except:
+			no_exception = False
+			wit_result = "Exception: wit cannot recognize!"
+			#print(wit_result)
 
-        try:
-            #每日限額100單位,沒有language參數
-            houndify_result = r.recognize_houndify(audio,client_id=houndify_id,client_key=houndify_key)
-            if houndify_result == '':
-                no_exception = False
-                houndify_result = "Exception: houndify cannot recognize!"
-                #print(houndify_result)
-            else:
-                #print("houndify : ", houndify_result)
-                pass
-        except:
-            if no_exception == True:
-                exceed_quota = True
-            no_exception = False
-            houndify_result = "Exception: houndify cannot recognize!"
-            #print(houndify_result)
+		try:
+			#每日限額100單位,沒有language參數
+			houndify_result = r.recognize_houndify(audio,client_id=houndify_id,client_key=houndify_key)
+			if houndify_result == '':
+				no_exception = False
+				houndify_result = "Exception: houndify cannot recognize!"
+				#print(houndify_result)
+			else:
+				#print("houndify : ", houndify_result)
+				pass
+		except:
+			if no_exception == True:
+				exceed_quota = True
+			no_exception = False
+			houndify_result = "Exception: houndify cannot recognize!"
+			#print(houndify_result)
 
-        #print("-------------------------------")
-        results = [google_result, ibm_result, wit_result, houndify_result]
-        return results, no_exception, exceed_quota
+		#print("-------------------------------")
+		results = [google_result, ibm_result, wit_result, houndify_result]
+		store_result[AudioFile] = results, no_exception, exceed_quota
+		return results, no_exception, exceed_quota
 
 
 def to_final_result(API_results, weight, threshold):
-    S = StringAlign()
-    S += API_results
+	S = StringAlign()
+	S += API_results
 
-    p = give_param('james')
-    S.evaluate(p)
-    #x = S.big_anchor_concat_james()
-    x = S.big_anchor_concat_heuristic(p)
-    x = x['word_set']
-    alignment = S.str_big_anchor()
-    final_result = ' '.join(S.final_result(weight, threshold))
-    
-    return alignment, final_result
+	p = give_param('james')
+	S.evaluate(p)
+	#x = S.big_anchor_concat_james()
+	x = S.big_anchor_concat_heuristic(p)
+	x = x['word_set']
+	alignment = S.str_big_anchor()
+	final_result = ' '.join(S.final_result(weight, threshold))
+	
+	return alignment, final_result
